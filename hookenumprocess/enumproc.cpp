46aac5dcb99cfac63c19c138bd60f44b33026a7f
billy17 <himanshup@cdac.in>
2016-01-21 11:29:46 +0530

    alpha release code

--- hookenumprocess/enumproc.cpp
+++ hookenumprocess/enumproc.cpp
@@ -13,8 +13,6 @@
 #include <stdio.h>
 #include <windows.h>
 #include <psapi.h>
-
-
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -22,6 +20,7 @@ using namespace std;
 #include "detours.h"
 #pragma comment(lib, "detours.lib")
 #pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "psapi.lib")
 
 LARGE_INTEGER freq;
 
@@ -37,7 +36,7 @@ typedef struct _Record
 	char				isFree;
 } Record;
 
-//Record vRecord[9999];
+Record vRecord[9999];
 
 
 int heapcnt=0;
@@ -48,22 +47,22 @@ int heapcnt=0;
 //}
 
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
-//FILE *fp_heap ;
+FILE *fp_heap ;
 FILE *fp_all;
 
-//static BOOL (WINAPI * TrueHeapFree)(HANDLE hHeap, DWORD dwFlags, LPVOID lpMem) = HeapFree;
-//static BOOL (WINAPI * TrueHeapDestroy)(HANDLE hHeap) = HeapDestroy;
+static BOOL (WINAPI * TrueHeapFree)(HANDLE hHeap, DWORD dwFlags, LPVOID lpMem) = HeapFree;
+static BOOL (WINAPI * TrueHeapDestroy)(HANDLE hHeap) = HeapDestroy;
 
 
-//#if _MSC_VER < 1300
-//LPVOID (WINAPI *
-//        TrueHeapAlloc)(HANDLE hHeap, DWORD dwFlags, DWORD dwBytes)
-//    = HeapAlloc;
-//#else
-//LPVOID (WINAPI *
-//        TrueHeapAlloc)(HANDLE hHeap, DWORD dwFlags, DWORD_PTR dwBytes)
-//    = HeapAlloc;
-//#endif
+#if _MSC_VER < 1300
+LPVOID (WINAPI *
+        TrueHeapAlloc)(HANDLE hHeap, DWORD dwFlags, DWORD dwBytes)
+    = HeapAlloc;
+#else
+LPVOID (WINAPI *
+        TrueHeapAlloc)(HANDLE hHeap, DWORD dwFlags, DWORD_PTR dwBytes)
+    = HeapAlloc;
+#endif
 
 static BOOL (WINAPI * TrueEnumProcesses)(DWORD *pProcessIds, DWORD cb, DWORD *pBytesReturned) = EnumProcesses;
 static HANDLE (WINAPI * TrueOpenProcess)(DWORD dwDesiredAccess, BOOL bInheritHandle, DWORD dwProcessId) = OpenProcess;
@@ -141,7 +140,7 @@ BOOL __declspec(dllexport) WINAPI MPEnumProcesses(DWORD *pProcessIds, DWORD cb, 
 	
 	// send info to thread in u_malanalyze
 	
-	fprintf(fp_all, "Enumerates-Processes \n");
+	fprintf(fp_all, "Enumerating Processes \n");
 
     return ret;
 }
@@ -155,13 +154,10 @@ HANDLE __declspec(dllexport) WINAPI MPOpenProcess(DWORD dwDesiredAccess, BOOL bI
 	dwCurrId = GetCurrentProcessId();
 	hProcess = TrueOpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId);
 	
-	if(dwCurrId == dwProcessId)		// this is insignificant
-		return hProcess;
-
 	if(GetProcessImageFileName(hProcess, ImageFileName, 1023))
-			fprintf(fp_all, "%d, Opening Process (%d), %ws \n", dwCurrId, dwProcessId, ImageFileName);
+			fprintf(fp_all, "%d. Opening Process (%d) %ws \n", dwCurrId, dwProcessId, ImageFileName);
 		else
-			fprintf(fp_all, "%d, Opening Process (%d)\n", dwCurrId, dwProcessId);
+			fprintf(fp_all, "%d. Opening Process (%d)\n", dwCurrId, dwProcessId);
 
 	return hProcess;
 }
@@ -181,15 +177,10 @@ BOOL __declspec(dllexport) WINAPI MPReadProcessMemory(HANDLE hProcess,
 	dwProcessId = GetProcessId(hProcess);
 	dwCurrId = GetCurrentProcessId();
 
-	if(dwCurrId == dwProcessId){
-		fprintf(fp_all, "read-own-process-memory \n");
-		return ret;
-	}
-
 	if(GetProcessImageFileName(hProcess, ImageFileName, 1023))
-		fprintf(fp_all, "read-process-memory, %d,(%d), %ws \n", dwCurrId, dwProcessId, ImageFileName);
+		fprintf(fp_all, "%d. Reading Process Memory (%d) %ws \n", dwCurrId, dwProcessId, ImageFileName);
 	else
-		fprintf(fp_all, "read-process-memory, %d, %d \n", dwCurrId, dwProcessId);
+		fprintf(fp_all, "%d. Reading Process Memory %d \n", dwCurrId, dwProcessId);
 
 	return ret;
 }
@@ -210,91 +201,86 @@ BOOL __declspec(dllexport) WINAPI  MPWriteProcessMemory(HANDLE hProcess,
 	dwProcessId = GetProcessId(hProcess);
 	dwCurrId = GetCurrentProcessId();
 
-	if(dwCurrId == dwProcessId){
-		fprintf(fp_all, "write-own-process-memory \n");
-		return ret;
-	}
-
 	if(GetProcessImageFileName(hProcess, ImageFileName, 1023))
-		fprintf(fp_all, "write-process-memory, %d, (%d), %ws \n", dwCurrId, dwProcessId, ImageFileName);
+		fprintf(fp_all, "%d. Writing Process  Memory (%d) %ws \n", dwCurrId, dwProcessId, ImageFileName);
 	else
-		fprintf(fp_all, "write-process-memory, %d, (%d)\n", dwCurrId, dwProcessId);
+		fprintf(fp_all, "%d. Writing Process  Memory %d \n", dwCurrId, dwProcessId);
 
 	return ret;
 }
 
-//BOOL _declspec(dllexport) MPHeapDestroy(HANDLE hHeap)
-//{
-//	int counter=0;
-//
-//	while(counter <= heapcnt)
-//	{
-//		if(  (vRecord[counter].HeapHandle == hHeap) && (vRecord[counter].isFree == 0) ) 
-//		{
-//			QueryPerformanceCounter(&vRecord[counter].HeapEnd);
-//			double delta_milliseconds = (double)(vRecord[counter].HeapEnd.QuadPart - vRecord[counter].HeapStart.QuadPart) / freq.QuadPart * 1000.0;
-//			//fprintf(fp_heap, "%0.16p, %0.8d, %0.16f, d\n", vRecord[counter].HeapAddress, vRecord[counter].HeapSize, delta_milliseconds);
-//			vRecord[counter].isFree = 1;
-//		}
-//		
-//		++counter;
-//	}
-//
-//	return TrueHeapDestroy(hHeap);
-//}
+BOOL _declspec(dllexport) MPHeapDestroy(HANDLE hHeap)
+{
+	int counter=0;
 
-//BOOL __declspec(dllexport) WINAPI MPHeapFree(HANDLE hHeap, DWORD dwFlags, LPVOID lpMem)
-//{
-//	int counter=0;
-//	while(counter <= heapcnt)
-//	{
-//		if ( (lpMem!=0) && (vRecord[counter].HeapAddress == lpMem) && (vRecord[counter].isFree == 0) )
-//		{
-//			QueryPerformanceCounter(&vRecord[counter].HeapEnd);
-//		
-//			double delta_milliseconds = (double)(vRecord[counter].HeapEnd.QuadPart - vRecord[counter].HeapStart.QuadPart) / freq.QuadPart * 1000.0;
-//
-//			//fprintf(fp_heap, "%0.16p, %0.8d, %0.16f, f\n", lpMem, vRecord[counter].HeapSize, delta_milliseconds);
-//
-//			vRecord[counter].isFree = 1;
-//
-//			break;
-//		}
-//	
-//		++counter;
-//	} // while
-//
-//	// print to CSV file.
-//
-//
-//	return TrueHeapFree(hHeap, dwFlags, lpMem);
-//}
+	while(counter <= heapcnt)
+	{
+		if(  (vRecord[counter].HeapHandle == hHeap) && (vRecord[counter].isFree == 0) ) 
+		{
+			QueryPerformanceCounter(&vRecord[counter].HeapEnd);
+			double delta_milliseconds = (double)(vRecord[counter].HeapEnd.QuadPart - vRecord[counter].HeapStart.QuadPart) / freq.QuadPart * 1000.0;
+			fprintf(fp_heap, "%0.16p, %0.8d, %0.16f, d\n", vRecord[counter].HeapAddress, vRecord[counter].HeapSize, delta_milliseconds);
+			vRecord[counter].isFree = 1;
+		}
+		
+		++counter;
+	}
 
-//#if _MSC_VER < 1300
-//LPVOID __declspec(dllexport) WINAPI MPHeapAlloc(HANDLE hHeap, DWORD dwFlags, DWORD dwBytes)
-//#else
-//LPVOID __declspec(dllexport) WINAPI MPHeapAlloc(HANDLE hHeap, DWORD dwFlags, DWORD_PTR dwBytes)
-//#endif
-//{	
-//	LPVOID rv=0;
-//	
-//	rv = TrueHeapAlloc(hHeap, dwFlags, dwBytes);
-//
-//	if( (rv !=0) && ( dwBytes!=0 ) && (heapcnt<9999) )
-//	{
-//		vRecord[heapcnt].HeapHandle = hHeap;
-//		vRecord[heapcnt].HeapSize = dwBytes;
-//	
-//		QueryPerformanceCounter(&vRecord[heapcnt].HeapStart);
-//		
-//		vRecord[heapcnt].HeapAddress = rv;
-//		vRecord[heapcnt].cnt = heapcnt;
-//		vRecord[heapcnt].isFree = 0;
-//		++heapcnt;
-//	}
-//	
-//	return rv;
-//}
+	return TrueHeapDestroy(hHeap);
+}
+
+BOOL __declspec(dllexport) WINAPI MPHeapFree(HANDLE hHeap, DWORD dwFlags, LPVOID lpMem)
+{
+	int counter=0;
+	while(counter <= heapcnt)
+	{
+		if ( (lpMem!=0) && (vRecord[counter].HeapAddress == lpMem) && (vRecord[counter].isFree == 0) )
+		{
+			QueryPerformanceCounter(&vRecord[counter].HeapEnd);
+		
+			double delta_milliseconds = (double)(vRecord[counter].HeapEnd.QuadPart - vRecord[counter].HeapStart.QuadPart) / freq.QuadPart * 1000.0;
+
+			fprintf(fp_heap, "%0.16p, %0.8d, %0.16f, f\n", lpMem, vRecord[counter].HeapSize, delta_milliseconds);
+
+			vRecord[counter].isFree = 1;
+
+			break;
+		}
+	
+		++counter;
+	} // while
+
+	// print to CSV file.
+
+
+	return TrueHeapFree(hHeap, dwFlags, lpMem);
+}
+
+#if _MSC_VER < 1300
+LPVOID __declspec(dllexport) WINAPI MPHeapAlloc(HANDLE hHeap, DWORD dwFlags, DWORD dwBytes)
+#else
+LPVOID __declspec(dllexport) WINAPI MPHeapAlloc(HANDLE hHeap, DWORD dwFlags, DWORD_PTR dwBytes)
+#endif
+{	
+	LPVOID rv=0;
+	
+	rv = TrueHeapAlloc(hHeap, dwFlags, dwBytes);
+
+	if( (rv !=0) && ( dwBytes!=0 ) && (heapcnt<9999) )
+	{
+		vRecord[heapcnt].HeapHandle = hHeap;
+		vRecord[heapcnt].HeapSize = dwBytes;
+	
+		QueryPerformanceCounter(&vRecord[heapcnt].HeapStart);
+		
+		vRecord[heapcnt].HeapAddress = rv;
+		vRecord[heapcnt].cnt = heapcnt;
+		vRecord[heapcnt].isFree = 0;
+		++heapcnt;
+	}
+	
+	return rv;
+}
 
 BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
 {
@@ -308,19 +294,20 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
     }
 
     if (dwReason == DLL_PROCESS_ATTACH) {
-		fp_all=fopen("ignore.txt", "w");
-		//fp_heap=fopen("heap.csv", "a+");
+		fp_all=fopen("ignore.txt", "a+");
+		fp_heap=fopen("heap.csv", "a+");
 
         DetourRestoreAfterWith();
 
-        //printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"" Starting.\n");
+        printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
+               " Starting.\n");
         fflush(stdout);
 
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
   
-		//DetourAttach(&(PVOID&)TrueHeapAlloc, MPHeapAlloc);
-		//DetourAttach(&(PVOID&)TrueHeapFree, MPHeapFree);
+		DetourAttach(&(PVOID&)TrueHeapAlloc, MPHeapAlloc);
+		DetourAttach(&(PVOID&)TrueHeapFree, MPHeapFree);
 		
 		DetourAttach(&(PVOID&)TrueCreateProcess, MPCreateProcess);
 		DetourAttach(&(PVOID&)TrueEnumProcesses, MPEnumProcesses);
@@ -331,20 +318,20 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
         error = DetourTransactionCommit();
 
         if (error == NO_ERROR) {
-            /*printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
-                   " Detoured EnumProcesses().\n");*/
+            printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
+                   " Detoured EnumProcesses().\n");
         }
         else {
-            /*printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
-                   " Error detouring EnumProcesses(): %d\n", error);*/
+            printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
+                   " Error detouring EnumProcesses(): %d\n", error);
         }
     }
     else if (dwReason == DLL_PROCESS_DETACH) {
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
 	
-		//DetourDetach(&(PVOID&)TrueHeapAlloc, MPHeapAlloc);
-		//DetourDetach(&(PVOID&)TrueHeapFree, MPHeapFree);
+		DetourDetach(&(PVOID&)TrueHeapAlloc, MPHeapAlloc);
+		DetourDetach(&(PVOID&)TrueHeapFree, MPHeapFree);
 		//
 		DetourDetach(&(PVOID&)TrueCreateProcess, MPCreateProcess);
 		DetourDetach(&(PVOID&)TrueEnumProcesses, MPEnumProcesses);
@@ -354,10 +341,10 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
 
         error = DetourTransactionCommit();
 
-		//fclose(fp_heap);
+		fclose(fp_heap);
 		fclose(fp_all);
-        //printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
-        //       " Removed EnumProcesses() (result=%d), \n", error);
+        printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
+               " Removed EnumProcesses() (result=%d), \n", error);
         fflush(stdout);
     }
     return TRUE;
